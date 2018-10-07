3700410ecfb164177b69a77bd98d992e4fd53aca
rodferro <rod_ferro@msn.com>
2018-10-07 12:18:57 -0300

    Add python script

---
+++ 07_first_mlp.py
@@ -0,0 +1,31 @@
+# Create a MLP in Keras
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+
+# fix random seed for reproducibility
+np.random.seed(7)
+
+# load dataset
+filename = 'pima-indians-diabetes.csv'
+dataset = np.loadtxt(filename, delimiter=',')
+
+# split into input (X) and output (y) variables
+X = dataset[:, :8]
+y = dataset[:, 8]
+
+# create model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# fit the model
+model.fit(X, y, epochs=150, batch_size=10)
+
+# evaluate the model
+scores = model.evaluate(X, y)
+print('\n%s: %.2f%%' % (model.metrics_names[1], scores[1] * 100))
