fac7fc2ed35a2ac254a53d1879ebbd3fce28a1c7
rodferro <rod_ferro@msn.com>
2018-10-09 23:19:00 -0300

    Convert indentation to spaces

--- 09_sklearn_cross_validation.py
+++ 09_sklearn_cross_validation.py
@@ -8,15 +8,15 @@ import numpy as np
 
 # function to create model, required for KerasClassifier
 def create_model():
-	# create
-	model = Sequential()
-	model.add(Dense(12, input_dim=8, activation='relu'))
-	model.add(Dense(8, activation='relu'))
-	model.add(Dense(1, activation='sigmoid'))
+    # create
+    model = Sequential()
+    model.add(Dense(12, input_dim=8, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
 
-	# compile
-	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
+    # compile
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
 # fix random seed for reproducibility
 seed = 7
