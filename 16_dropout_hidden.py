e896db09ae74473662b1bf4129c9202556a61a18
rodferro <rod_ferro@msn.com>
2018-10-16 22:59:02 -0300

    Add python script

---
+++ 16_dropout_hidden.py
@@ -0,0 +1,55 @@
+# Example of Dropout on the Sonar Dataset: Hidden Layer
+import numpy as np
+from pandas import read_csv
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+# fix random seed for reproducibility
+seed = 7
+np.random.seed(seed)
+
+# load dataset
+filename = 'sonar.csv'
+dataframe = read_csv(filename, header=None)
+dataset = dataframe.values
+
+# split into input (X) and output (y) variables
+X = dataset[:, :60].astype(float)
+y = dataset[:, 60]
+
+# encode class values as integers
+encoder = LabelEncoder()
+encoder.fit(y)
+encoded_y = encoder.transform(y)
+
+# dropout in hidden layers with weight constraint
+def create_model():
+    # create
+    model = Sequential()
+    model.add(Dense(60, input_dim=60, kernel_initializer='normal', activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(Dropout(0.2))
+    model.add(Dense(30, kernel_initializer='normal', activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(Dropout(0.2))
+    model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+
+    # compile
+    sgd = SGD(lr=0.1, momentum=0.9, decay=0.0, nesterov=False)
+    model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    return model
+
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasClassifier(build_fn=create_model, epochs=300, batch_size=16, verbose=0)))
+pipeline = Pipeline(estimators)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(pipeline, X, encoded_y, cv=kfold)
+print('Hidden: %.2f%% (%.2f%%)' % (results.mean() * 100, results.std() * 100))
