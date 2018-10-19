b8ae35c8260fedce9aaf29badc74e4ed13d794ac
rodferro <rod_ferro@msn.com>
2018-10-18 21:06:21 -0300

    Add python script

--- 17_decay_drop_based.py
+++ 17_decay_drop_based.py
@@ -1,10 +1,20 @@
-# Time Based Learning Rate Decay
+# Drop-Based Learning Rate Decay
 from pandas import read_csv
 import numpy as np
+import math
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
 from sklearn.preprocessing import LabelEncoder
+from keras.callbacks import LearningRateScheduler
+
+# learning rate schedule
+def step_decay(epoch):
+    initial_lrate = 0.1
+    drop = 0.5
+    epochs_drop = 10.0
+    lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
+    return lrate
 
 # fix random seed for reproducibility
 np.random.seed(7)
@@ -28,12 +38,13 @@ model = Sequential()
 model.add(Dense(34, input_dim=34, kernel_initializer='normal', activation='relu'))
 model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
 
-# Compile model
-epochs = 50
-learning_rate = 0.1
-decay_rate = learning_rate / epochs
-sgd = SGD(lr=learning_rate, momentum=0.8, decay=decay_rate, nesterov=False)
+# compile model
+sgd = SGD(lr=0.0, momentum=0.9, decay=0.0, nesterov=False)
 model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-# Fit the model
-model.fit(X, y, validation_split=0.33, epochs=epochs, batch_size=28, verbose=2)
+# learning schedule callback
+lrate = LearningRateScheduler(step_decay)
+callbacks_list = [lrate]
+
+# fit the model
+model.fit(X, y, validation_split=0.33, epochs=50, batch_size=28, callbacks=callbacks_list, verbose=2)
